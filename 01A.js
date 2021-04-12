4eb7c952df390490665f65866d800b0df122b745
yangc95 <72277593+yangc95@users.noreply.github.com>
2021-04-12 18:06:24 -0400

    Create template for pairboarding

---
+++ 01A.js
@@ -0,0 +1,17 @@
+// Write a method, digital_root(num). 
+// It should sum the digits of a positive integer. 
+// If it is greater than or equal to 10, sum the digits of the resulting number. 
+// Keep repeating until there is only one digit in the result, called the "digital root". 
+// Do not use string conversion within your method.
+
+// caesar_cipher
+// Write a function that takes a message and an increment amount and outputs the same letters shifted by that amount in the alphabet. 
+// Assume lowercase and no punctuation. Preserve spaces.
+
+// Write a function, longest_common_substring(str1, str2) that takes two strings and returns the longest common substring. 
+// A substring is defined as any consecutive slice of letters from another string.
+// Bonus: solve it in O(m * n) using O(m * n) extra space. (Hint: the solution involves dynamic programming which will be introduced later in the course.)
+
+// sum_rec
+// Write a function that takes an array of integers and returns their sum. Use recursion.
+
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Pairboarding
 
-
-
 ## 01A
 Introduction
 Walk me through your background.
@@ -13,3 +11,13 @@ Your answer should tell the interviewer the following:
 Summarize the situation that led to the conflict
 What steps did you take to resolving the conflict?
 Did your resolution work? How was the project and/or your relationship with the coworker improved in the process of resolving this conflict?
+
+## 01B
+Walk me through your background.
+NB: this is your cue to do your personal pitch. Assume you are applying to Twitter.
+
+Tell me about a time when you took initiative.
+
+Summarize the project you took initiative on
+Describe what you did -- be concise, but give just enough detail so that your interviewer (who just met you) can follow easily
+What was the result? How was the project/situation improved because of your actions?
