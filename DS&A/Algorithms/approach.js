2e77177abc23324f27e71f0e93706919bc010cf9
yangc95 <72277593+yangc95@users.noreply.github.com>
2021-05-07 16:51:52 -0400

    Add patterns file to algos

--- DS&A/Algorithms/approach.js
+++ DS&A/Algorithms/approach.js
@@ -36,14 +36,29 @@
 function charCount(str) {
     let obj = {};
     str.split("").forEach(char => {
-        if (obj[char] > 0) obj[char]++;
-        if (!obj[char]) obj[char] = 1;
+        code = char.toLowerCase();
+        if (/[a-z0-9]/.test(char)) {
+            obj[char] = ++obj[char] || 1;
+            // if (obj[char] > 0) obj[char]++;
+            // if (!obj[char]) obj[char] = 1;
+        }
     }) 
     return obj;
 }
 
-// console.log(charCount("hello"))
+// function isAlphaNumeric(char) {
+//     var code = char.charCodeAt(0);
+//     if (!(code > 47 && code < 58) && // numeric (0-9)
+//         !(code > 64 && code < 91) && // upper alpha (A-Z)
+//         !(code > 96 && code < 123)) { // lower alpha (a-z)
+//             return false;
+//         }
+//     return true;
+// }
+
+console.log(charCount("helLo36ADLLL"))
 
 // =======================================================
 // BREAK DOWN THE PROBLEM INTO SOLVEABLE PARTS
+// LOOK BACK AND REFACTOR
 // =======================================================
