ea03e0c2e83bd92b42928dae8f72ae9b99791bfe
yangc95 <72277593+yangc95@users.noreply.github.com>
2021-05-06 23:59:04 -0400

    Last commit of day, add object method big o

---
+++ DS&A/Performance.js
@@ -0,0 +1,16 @@
+// Adding elements to the beginning of an array is costly
+
+// Refresher - an object is an unordered data structure
+// made up of key value pairs
+// BigO for Objects:
+//  insertion -> O(1)
+//  removal -> O(1)
+//  searching -> O(n)
+//  access -> O(1)
+
+// Object Methods
+//  .keys -> O(n)
+//  .values -> O(n)
+//  .entries -> O(n)
+//  hasOwnProperty -> O(1)
+
