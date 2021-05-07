c287484727601cfedb27d89b3489ba8591cc85fb
yangc95 <72277593+yangc95@users.noreply.github.com>
2021-05-07 02:20:18 -0400

    Edit performance file

--- DS&A/Performance.js
+++ DS&A/Performance.js
@@ -13,4 +13,21 @@
 //  .values -> O(n)
 //  .entries -> O(n)
 //  hasOwnProperty -> O(1)
+___________________________________________________________
 
+// Arrays, ordered lists
+// there is intrinsic order to the data
+// each element in the array has an index
+
+// BigO for Array:
+//  insertion -> it depends
+//  removal -> depends
+//  searching -> O(n)
+//  access -> O(1)
+
+// Array Methods
+//  
+
+
+// Singly linked list
+// Doubly liked list
